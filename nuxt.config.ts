29d3315a5d1b6099c59f4b85b5bb0ef7a19d4ce6
akbarsaputrait <akbarsaputrait@outlook.com>
2024-12-14 23:50:48 +0700

    fix public path and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-
+  dir: {
+    public: "public",
+  },
   typescript: {
     typeCheck: true,
   },
@@ -27,6 +29,100 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/assets/favicon.ico",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "57x57",
+          href: "/assets/apple-icon-57x57.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "60x60",
+          href: "/assets/apple-icon-60x60.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "72x72",
+          href: "/assets/apple-icon-72x72.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "76x76",
+          href: "/assets/apple-icon-76x76.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "114x114",
+          href: "/assets/apple-icon-114x114.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "120x120",
+          href: "/assets/apple-icon-120x120.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "144x144",
+          href: "/assets/apple-icon-144x144.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "152x152",
+          href: "/assets/apple-icon-152x152.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/assets/apple-icon-180x180.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "192x192",
+          href: "/assets/android-icon-192x192.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/assets/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "96x96",
+          href: "/assets/favicon-96x96.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/assets/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/assets/manifest.json",
+        },
+      ],
+      meta: [
+        {
+          name: "msapplication-TileColor",
+          content: "#ffffff",
+        },
+        {
+          name: "msapplication-TileImage",
+          content: "/assets/ms-icon-144x144.png",
+        },
+        {
+          name: "theme-color",
+          content: "#ffffff",
+        },
+      ],
     },
   },
 
